c302533830098ec95bd9b960395727ca0f3ac99a
Alexander Lyon <arlyon@me.com>
2017-02-26 15:38:55 +0100

    json format

--- src/handlers/systemd.go
+++ src/handlers/systemd.go
@@ -6,14 +6,45 @@ import (
 	"os/exec"
 	"fmt"
 	"strings"
+	"src/github.com/fatih/structs"
+	"encoding/json"
 )
+
+type Status struct {
+	Loaded bool
+	Active bool
+	Substate string
+}
 
 func Systemd(w http.ResponseWriter, r *http.Request) {
 	conf := config.GetConfig()
 
-	output, err := exec.Command("/bin/sh", "-c", "sudo systemctl list-units | grep '"+strings.Join(conf.ModSystemd.Services, "\\|")+"'").Output()
+	systemd, err := exec.Command("/bin/sh", "-c", "sudo systemctl list-units | grep '"+strings.Join(conf.ModSystemd.Services, "\\|")+"'").Output()
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Fprint(w, string(output)+"\n")
+
+	response := make(map[string]interface{})
+
+	for _,element := range strings.Split(string(systemd[1:len(systemd)]), "\n") {
+		args := strings.Fields(element)
+
+		isloaded := false
+		if args[1] == "loaded" {
+			isloaded = true
+		}
+
+		isactive := false
+		if args[2] == "active" {
+			isactive = true
+		}
+
+		service := structs.Map(Status{isloaded, isactive, args[3]})
+		response[args[0]] = service
+	}
+
+	responsebody, _ := json.Marshal(response)
+	responsestring := string(responsebody)
+
+	fmt.Fprint(w, responsestring)
 }
