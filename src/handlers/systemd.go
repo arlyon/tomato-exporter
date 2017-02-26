add4e2c53a347729fde31f78e243947720eb0a4f
Alexander Lyon <arlyon@me.com>
2017-02-26 15:12:02 +0100

    change to units

--- src/handlers/systemd.go
+++ src/handlers/systemd.go
@@ -5,18 +5,15 @@ import (
 	"../config"
 	"os/exec"
 	"fmt"
+	"strings"
 )
 
 func Systemd(w http.ResponseWriter, r *http.Request) {
-
 	conf := config.GetConfig()
 
-	for _,value := range conf.ModSystemd.Services {
-		fmt.Println(value)
-		output, err := exec.Command("/bin/sh", "-c", "sudo systemctl status "+value).Output()
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Fprint(w, string(output)+"\n")
+	output, err := exec.Command("/bin/sh", "-c", "sudo systemctl list-units | grep '"+strings.Join(conf.ModSystemd.Services, "\\|")+"'").Output()
+	if err != nil {
+		fmt.Println(err)
 	}
+	fmt.Fprint(w, string(output)+"\n")
 }
